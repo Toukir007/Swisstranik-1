74976b89a14aebb17e76dc201c3d9526a2674f30
THOKIR AHAMMED <thokirahammed3@gmail.com>
2024-07-27 17:12:25 +0000

    Initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/",
-      accounts: ["35a061d8230be94c510f8f9920da5fba6cb3ec4f7bb85298f2d3bf03af7087a8"],
+      accounts: [""],
     },
   },
 };
